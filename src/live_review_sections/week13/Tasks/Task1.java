225ee82755e431584e099ec9a833339d69b71756
qayserily <muratbayram2005@gmail.com>
2021-06-26 20:14:30 +0300

    week13 review ended

---
+++ src/live_review_sections/week13/Tasks/Task1.java
@@ -0,0 +1,39 @@
+package live_review_sections.week13.Tasks;
+
+public class Task1 {
+}
+
+
+/*
+Shape Task:
+	interface Volume:
+		volume();
+
+	abstract Shape
+			name
+			area();
+			perimeter();
+
+	Square extends Shape:
+		area(): a * a
+		perimeter(): 4 * a
+
+	Circle extends Shape
+			area():
+			perimeter():
+
+	Rectangle extends Shape
+			area():
+			perimeter():
+
+	Cube extends Shape implements Volume
+			area():
+			perimeter():
+			volume():
+
+	Cylinder extends Shape implements Volume
+			area():
+			perimeter():
+			volume():
+
+ */
---
+++ src/live_review_sections/week13/Tasks/Task2.java
@@ -0,0 +1,39 @@
+package live_review_sections.week13.Tasks;
+
+public class Task2 {
+}
+
+
+/*
+Car Task:
+	interface Fly:
+			fly();
+
+	interface AutoPilot:
+		selfDrive();
+
+	abstract Car
+		make, model, year, price, color....
+		start();
+
+
+	Toyota extends Car
+		start():
+
+	BMW extends Car
+		start():
+
+	Tesla extends Car implments AutoPilot
+		start():
+		selfDrive():
+
+	Nio extends Car implments AutoPilot
+		start():
+		selfDrive():
+
+	Cybertek extends Car implments AutoPilot, Fly
+		start():
+		selfDrive():
+		fly():
+
+ */
