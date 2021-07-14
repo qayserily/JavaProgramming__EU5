01ae52931085152e4a7b0ec6ee7f6ff3547316d6
qayserily <muratbayram2005@gmail.com>
2021-07-14 17:09:44 +0300

    day 48 finished

---
+++ src/daily_videos/day47_collections_part2/LoopSet.java
@@ -0,0 +1,26 @@
+package daily_videos.day47_collections_part2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LoopSet {
+    public static void main(String[] args) {
+        Set<Integer> numSet = new HashSet<>();
+        numSet.add(50);
+        numSet.add(543);
+        numSet.add(1150);
+        numSet.add(550);
+        numSet.add(570);
+        numSet.add(93);
+        numSet.add(5000);
+        numSet.add(950);
+
+        for(Integer n : numSet) {
+            System.out.print(n + "|");
+        }
+
+        System.out.println();
+
+        numSet.forEach(n -> System.out.print(n + "|"));
+    }
+}
